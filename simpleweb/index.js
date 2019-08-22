7490794319c6d857c63f5152fc6fbf359f5b804b
Dom Hallan <dom@codefuldom.com>
2019-08-22 12:25:34 -0500

    :sparkles: added change to get route

--- simpleweb/index.js
+++ simpleweb/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hi there')
+  res.send('Hi, Dom? Express is trash. Write this shit in node.')
 })
 
 app.listen(8081, () => {
